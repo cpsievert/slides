e1f80a62f7b5dfce644f0022f4f9717f2e503eaf
Carson Sievert <cpsievert1@gmail.com>
2015-10-27 19:47:19 +1100

    better intro

--- intro/yourTurn1.R
+++ intro/yourTurn1.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 l <- loess(unemploy / pop ~ as.numeric(date), economics)
-economics$yhat <- fitted(l)
+economics$yhat <- l$fitted
 ggplot(economics) + 
   geom_line(aes(date, unemploy / pop)) +
   geom_line(aes(date, yhat), color = "blue")
